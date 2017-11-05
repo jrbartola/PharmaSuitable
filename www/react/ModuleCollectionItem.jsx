b4513bc0ae62d62d6fa9a1616245ab87c9700ad5
Jesse Bartola <jrbartola@gmail.com>
2017-11-04 21:15:16 -0400

    Heart rate

--- www/react/ModuleCollectionItem.jsx
+++ www/react/ModuleCollectionItem.jsx
@@ -15,7 +15,7 @@ class ModuleCollectionItem extends React.Component {
 			<li className={"collection-item avatar"} >
               <img src={"img/" + this.props.image} alt="" className="circle" />
               <span className="title">{this.props.title}</span>
-              <p><i className="material-icons">favorite</i>{this.props.data}</p>
+              <p><i className="material-icons">favorite</i>{this.getRandomInt(72, 85)}</p>
               {/*<a href="#!" className="secondary-content"><i className="material-icons">grade</i></a>*/}
             </li>
 		)
